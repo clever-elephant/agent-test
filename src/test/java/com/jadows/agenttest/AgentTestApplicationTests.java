024b50d65c1e402647c9a4cc17fa56444caa5b52
wangzida <17355477009@163.com>
2026-01-20 15:56:27 +0800

    first commit

---
+++ src/test/java/com/jadows/agenttest/AgentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadows.agenttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
